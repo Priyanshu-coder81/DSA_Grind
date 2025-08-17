8e4587b4e89cc209d6798c196a3b3969519aa7b3
Priyanshu Verma <vpriyanshu864@gmail.com>
2025-08-17 22:37:17 +0530

    Add solution for Maximum 69 Number problem

---
+++ Maximum69numbers.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int maximum69Number (int num) {
+        
+        int n = num;
+        StringBuilder sb = new StringBuilder();
+
+        while(n>0) {
+            int r = n%10;
+            sb.append(r);
+            n=n/10;
+        }
+
+        n = sb.length();
+        num=0;
+        int k= 0;
+
+        for(int i = n-1; i>=0; i--) {
+           char c = sb.charAt(i);
+            if(k==0 && c=='6') {
+            num=num*10 + 9;
+            k++;
+            continue;
+           }
+           num=(num*10)+(c-'0');
+        }
+        return num;
+    }
+} Maximum69numbers {
+    
+}
