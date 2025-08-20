1dc6ecc15e33e6d5a44efb403f3d2eeb1436440f
Priyanshu Verma <vpriyanshu864@gmail.com>
2025-08-20 21:13:50 +0530

    Add implementation for counting squares in a binary matrix

---
+++ DP/countSquares.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int dp[][] = new int[m][n];
+        int ans = 0;
+
+        for(int i = 0; i<m; i++) {
+            dp[i][0] = matrix[i][0];
+            ans += dp[i][0];
+        }
+        for(int i = 1; i<n; i++) {
+            dp[0][i] = matrix[0][i];
+            ans += dp[0][i];
+        }
+        for(int i = 1; i<m; i++) {
+            for(int j = 1; j<n; j++) {
+                if(matrix[i][j] == 1) {
+                    int min = Math.min(dp[i-1][j],dp[i-1][j-1]);
+                    min = Math.min(min,dp[i][j-1]);
+
+                    dp[i][j] = min+1;
+                    ans+= dp[i][j];
+                }
+                else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return ans;
+    }
+}
